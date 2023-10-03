c9f494ada1406478e1e8759386434603e7d1043c
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-03 19:13:47 +0530

    Time: 29 ms (77.21%), Space: 49 MB (94.85%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,33 +1,45 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int[] pointers = new int[nums.size()];
-        int minRange = Integer.MAX_VALUE;
-        int[] result = new int[2];
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparing(i -> nums.get(i).get(pointers[i])));
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
         int max = Integer.MIN_VALUE;
-        
+
         for (int i = 0; i < nums.size(); i++) {
-            minHeap.offer(i);
-            max = Math.max(max, nums.get(i).get(0));
-        }
-        
-        while (true) {
-            int minListIdx = minHeap.poll();
-            int minVal = nums.get(minListIdx).get(pointers[minListIdx]);
-            if (max - minVal < minRange) {
-                minRange = max - minVal;
-                result[0] = minVal;
-                result[1] = max;
+            List<Integer> list = nums.get(i);
+            if (!list.isEmpty()) {
+                pq.add(new int[]{list.get(0), i, 0});
+                max = Math.max(max, list.get(0));
             }
-            pointers[minListIdx]++;
-            if (pointers[minListIdx] == nums.get(minListIdx).size()) {
-                break;
-            }
-            minHeap.offer(minListIdx);
-            max = Math.max(max, nums.get(minListIdx).get(pointers[minListIdx]));
         }
-        
-        return result;
+
+        int minR = Integer.MAX_VALUE, maxR = Integer.MIN_VALUE;
+        int minDiff = Integer.MAX_VALUE;
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int i = curr[1];
+            int j = curr[2];
+            int min = curr[0];
+
+            if (max - min < minDiff) {
+                minDiff = max - min;
+                minR = min;
+                maxR = max;
+            }
+
+            if (j + 1 < nums.get(i).size()) {
+                int nextVal = nums.get(i).get(j + 1);
+                if (nextVal > max) {
+                    max = nextVal;
+                }
+                
+               
+
+                pq.add(new int[]{nextVal, i, j + 1});
+            } else {
+                 return new int[]{minR, maxR};
+            }
+        }
+
+        return new int[]{minR, maxR};
     }
 }
