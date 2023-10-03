1253f5c0cb26462b88b99fd90d89cf0e1a6c327d
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-03 19:05:18 +0530

    Time: 35 ms (44.12%), Space: 49.4 MB (77.35%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.size(); i++) {
+            List<Integer> list = nums.get(i);
+            if (!list.isEmpty()) {
+                pq.add(new int[]{list.get(0), i, 0});
+                max = Math.max(max, list.get(0));
+            }
+        }
+
+        int minR = Integer.MAX_VALUE, maxR = Integer.MIN_VALUE;
+        int minDiff = Integer.MAX_VALUE;
+
+        while (pq.size() == nums.size() && nums.get(pq.peek()[1]).size() > pq.peek()[2]) {
+            int[] curr = pq.poll();
+            int i = curr[1];
+            int j = curr[2];
+            int min = curr[0];
+
+            if (max - min < minDiff) {
+                minDiff = max - min;
+                minR = min;
+                maxR = max;
+            }
+
+            if (j + 1 < nums.get(i).size()) {
+                int nextVal = nums.get(i).get(j + 1);
+                if (nextVal > max) {
+                    max = nextVal;
+                }
+                pq.add(new int[]{nextVal, i, j + 1});
+            }
+        }
+
+        return new int[]{minR, maxR};
+    }
+}
