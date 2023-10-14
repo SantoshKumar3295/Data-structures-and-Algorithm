1244317d6e7d95248f9a031085e633e41d469654
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-14 08:10:31 +0530

    Time: 8 ms (38.51%), Space: 40.6 MB (52.3%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,21 +1,53 @@
-class Solution {
+public class Solution {
     public int longestSubstring(String s, int k) {
-        return longestSubstringUtil(s, 0, s.length(), k);
-    }
 
-    int longestSubstringUtil(String s, int start, int end, int k) {
-        if (end < k) return 0;
-        int[] countMap = new int[26];
-        // update the countMap with the count of each character
-        for (int i = start; i < end; i++)
-            countMap[s.charAt(i) - 'a']++;
-        for (int mid = start; mid < end; mid++) {
-            if (countMap[s.charAt(mid) - 'a'] >= k) continue;
-            int midNext = mid + 1;
-            while (midNext < end && countMap[s.charAt(midNext) - 'a'] < k) midNext++;
-            return Math.max(longestSubstringUtil(s, start, mid, k),
-                    longestSubstringUtil(s, midNext, end, k));
+        int max = 0;
+        
+
+        for(int currUnique = 1; currUnique <= 26; currUnique++) {
+
+            int unique = 0;
+            int freq[] = new int[27];
+            int atLeastK = 0;
+
+            int start = 0, end = 0;
+
+            while(end < s.length()) {
+
+                if(atLeastK == unique) {
+                    max = Math.max(max, end-start);
+                }
+
+                if(unique <= currUnique) {
+
+                    int c = s.charAt(end)-'a';
+
+                    if(freq[c] == 0) unique++;
+
+                    freq[c]++;
+
+                    if(freq[c] == k) atLeastK++;
+
+                    end++;
+
+                } else {
+
+                    int c = s.charAt(start)-'a';
+
+                    if(freq[c] == k) atLeastK--;
+
+                    freq[c]--;
+
+                    if(freq[c] == 0) unique--;
+
+                    start++;
+                }
+
+                if(atLeastK == unique && currUnique == unique) {
+                    max = Math.max(max, end-start);
+                }
+            }
         }
-        return (end - start);
+        return max;
     }
 }
