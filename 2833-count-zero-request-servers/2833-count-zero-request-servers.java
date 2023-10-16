4fd93dad5d081f8b0b0d2d17344d396cef60124c
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-16 17:18:15 +0530

    Time: 41 ms (84.75%), Space: 84.7 MB (62.71%) - LeetHub

--- 2833-count-zero-request-servers/2833-count-zero-request-servers.java
+++ 2833-count-zero-request-servers/2833-count-zero-request-servers.java
@@ -1,45 +1,64 @@
+import java.util.*;
+
 class Solution {
-    public int[] countServers(int n, int[][] logs, int x, int[] queries) {
-        int m = logs.length;
-        List<int[]> vp = new ArrayList<>();
-        for (int[] log : logs) {
-            vp.add(new int[]{log[1], log[0]});
+    public int[] countServers(int n, int[][] servers, int x, int[] queries) {
+        int m = servers.length;
+        List<int[]> serversList = new ArrayList<>();
+        
+        // Convert servers array to a list of pairs: [time, serverId]
+        for (int[] server : servers) {
+            serversList.add(new int[]{server[1], server[0]});
         }
-        Collections.sort(vp, (a, b) -> a[0] - b[0]);
+        
+        // Sort serversList based on time
+        Collections.sort(serversList, (a, b) -> a[0] - b[0]);
 
         int q = queries.length;
-        Map<Integer, Integer> mp = new HashMap<>();
+        Map<Integer, Integer> serverMap = new HashMap<>();
 
-        int[] ans = new int[q];
-        List<int[]> time = new ArrayList<>();
+        int[] result = new int[q];
+        List<int[]> queryTimes = new ArrayList<>();
+        
+        // Convert queries array to a list of pairs: [queryTime, index]
         for (int i = 0; i < q; i++) {
-            time.add(new int[]{queries[i], i});
+            queryTimes.add(new int[]{queries[i], i});
         }
-        Collections.sort(time, (a, b) -> a[0] - b[0]);
+        
+        // Sort queryTimes based on queryTime
+        Collections.sort(queryTimes, (a, b) -> a[0] - b[0]);
 
-        int i = 0, j = 0;
-        for (int[] tm : time) {
-            int curtime = tm[0];
-            int ind = tm[1];
+        int startIdx = 0; // Start of the window
+        int endIdx = 0;   // End of the window
+        
+        for (int[] queryTime : queryTimes) {
+            int curTime = queryTime[0];
+            int queryIndex = queryTime[1];
 
-            int start = Math.max(0, curtime - x);
-            int end = curtime;
+            int startTime = Math.max(0, curTime - x);
+            int endTime = curTime;
 
-            while (j < m && vp.get(j)[0] <= end) {
-                mp.put(vp.get(j)[1], mp.getOrDefault(vp.get(j)[1], 0) + 1);
-                j++;
+            // Move endIdx until the time is not greater than endTime
+            while (endIdx < m && serversList.get(endIdx)[0] <= endTime) {
+                int serverId = serversList.get(endIdx)[1];
+                serverMap.put(serverId, serverMap.getOrDefault(serverId, 0) + 1);
+                endIdx++;
             }
-            while (i < m && vp.get(i)[0] < start) {
-                if (mp.getOrDefault(vp.get(i)[1], 0) == 1) {
-                    mp.remove(vp.get(i)[1]);
+
+            // Move startIdx until the time is not greater than or equal to startTime
+            while (startIdx < m && serversList.get(startIdx)[0] < startTime) {
+                int serverId = serversList.get(startIdx)[1];
+                if (serverMap.getOrDefault(serverId, 0) == 1) {
+                    serverMap.remove(serverId);
                 } else {
-                    mp.put(vp.get(i)[1], mp.get(vp.get(i)[1]) - 1);
+                    serverMap.put(serverId, serverMap.get(serverId) - 1);
                 }
-                i++;
+                startIdx++;
             }
-            ans[ind] = n - mp.size();
+
+            // Calculate the result for this query
+            result[queryIndex] = n - serverMap.size();
         }
 
-        return ans;
+        return result;
     }
 }
