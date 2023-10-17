651d7baaf35ad711b49aa8d03bc1028b0ac7c49b
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-17 08:09:25 +0530

    Time: 50 ms (36.13%), Space: 105.9 MB (11.77%) - LeetHub

--- 2833-count-zero-request-servers/2833-count-zero-request-servers.java
+++ 2833-count-zero-request-servers/2833-count-zero-request-servers.java
@@ -6,43 +6,28 @@ class Solution {
         for(int i = 0; i < queries.length; i++) qm.put(queries[i],i);
 
         Arrays.sort(logs, (a,b) -> a[1]-b[1]);
-
-        //System.out.println(Arrays.toString(queries));
-
         Arrays.sort(queries);
-        
-        int l = 0;
-        // for(int[]log : logs) {
-        //     System.out.println(l+" "+Arrays.toString(log));
-        //     l++;
-        // }
-
-      
 
         int []result = new int[queries.length];
 
         Arrays.fill(result, n);
 
-        int q = 0;
-        int right = 0, left = 0;
+        int q = 0, right = 0, left = 0;
 
         Map<Integer,Integer> set = new HashMap();
 
-        while(q< queries.length && left < logs.length) {
+        while(q < queries.length && left < logs.length) {
 
-           // System.out.println(left+" "+right+" "+qm.get(queries[q])+" "+set+" ");
-
-            if(q < queries.length && logs[left][1] >= queries[q]-x) {
-                if(right == logs.length) {
-                    set.put(logs[right-1][0], set.getOrDefault(logs[right-1][0],0)+1);
-                } else {
-                    while(right < logs.length && logs[right][1] <= queries[q]) {
-                        set.put(logs[right][0], set.getOrDefault(logs[right][0],0)+1);
-                        right++;
-                    }
+            if(logs[left][1] >= queries[q]-x) {
+                
+                while(right < logs.length && logs[right][1] <= queries[q]) {
+                    set.put(logs[right][0], set.getOrDefault(logs[right][0],0)+1);
+                    right++;
                 }
+        
                 result[qm.get(queries[q])] = n-set.size();
                 q++;
+                
             } else {
                 
                 if(set.containsKey(logs[left][0])) {
