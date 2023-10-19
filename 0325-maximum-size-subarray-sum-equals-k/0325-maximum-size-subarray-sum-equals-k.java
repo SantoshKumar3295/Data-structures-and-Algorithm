a7e192e0296c37d4d5b9770a16c04807d928f0ce
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-19 18:51:33 +0530

    Time: 65 ms (37.43%), Space: 69.9 MB (58.29%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
@@ -6,12 +6,14 @@ class Solution {
 
         int sum = 0;
         int max = 0;
+        
+        map.put(0,-1);
 
         for(int i = 0; i < nums.length; i++) {
 
             sum += nums[i];
 
-            if(sum == k) max = i+1;
+            //if(sum == k) max = i+1;
 
             if(map.containsKey(sum-k)) {
                 max = Math.max(max, i-map.get(sum-k));
