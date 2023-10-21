da85a6ce3661f65f74339460d34dce0ca03d2ec6
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-21 13:49:02 +0530

    Time: 14 ms (24.52%), Space: 44 MB (56.58%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,7 +1,43 @@
 class Solution {
     public int bestClosingTime(String customers) {
 
-        int n = customers.length()    ;
+        int n = customers.length();
+
+        int suffixSum = 0;
+
+        for(int i = n-1; i >= 0; i--) {
+            suffixSum += customers.charAt(i) == 'Y' ? 1 : 0;
+        }
+        
+        int prefixSum = 0;
+        int result = 0;
+        int minPenality = Integer.MAX_VALUE;
+
+        for(int i = 0; i < customers.length(); i++) {
+
+            int type = customers.charAt(i) == 'Y' ? 1 : 0;
+
+            suffixSum -= type;
+
+            int curr = (prefixSum+type) + suffixSum;
+
+            if(curr < minPenality) {
+                minPenality = curr;
+                result = i;
+            }
+
+            prefixSum += 1-type;;
+        }
+
+        if(prefixSum < minPenality) {
+            result = n;
+        }
+
+        return result;
+
+
+        /*
+        int n = customers.length();
 
         int [] suffixSum = new int [customers.length()+1];
 
@@ -16,7 +52,7 @@ class Solution {
         int minPenality = Integer.MAX_VALUE;
 
         for(int i = 0; i < customers.length(); i++) {
-            
+
             int type = customers.charAt(i) == 'Y' ? 1 : 0;
 
             int curr = (prefixSum+type) + suffixSum[i+1];
@@ -29,12 +65,12 @@ class Solution {
             prefixSum += 1-type;;
         }
 
-
         if(prefixSum < minPenality) {
             result = n;
         }
 
         return result;
+        */
 
     }
 }
