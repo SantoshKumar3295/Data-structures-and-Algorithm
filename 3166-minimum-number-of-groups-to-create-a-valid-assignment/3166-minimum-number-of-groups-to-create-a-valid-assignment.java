e8b65cc2495972ea30b97840a3764a3e8f4b016c
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-22 10:50:17 +0530

    Time: 352 ms (25%), Space: 61.2 MB (50%) - LeetHub

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.java
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.java
@@ -1,28 +1,36 @@
 public class Solution {
     public int minGroupsForValidAssignment(int[] nums) {
+        
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
         List<Integer> frequencies = new ArrayList<>();
+
         for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
             frequencies.add(entry.getValue());
         }
 
-        //Collections.sort(frequencies);
-
+        Collections.sort(frequencies);
+        
         int minGroups = nums.length;
+        
         for (int i = frequencies.get(0); i >= 1; --i) {
             boolean isValid = true;
             int totalGroups = 0;
 
             for (int frequency : frequencies) {
+        
                 final int quotient = frequency / i;
                 final int remainder = frequency % i;
 
+                
                 if (remainder > quotient) {
+                    System.out.println("group : "+i +", freq : "+ frequency+", q : "+quotient+", r :"+remainder);
                     isValid = false;
+                    totalGroups = 0;
                     break;
                 }
 
