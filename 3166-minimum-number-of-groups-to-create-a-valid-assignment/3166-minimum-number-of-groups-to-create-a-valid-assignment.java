c84316ee5690d871b26d6d3dce6371a479d5e1a2
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-22 10:29:43 +0530

    Time: 38 ms (100%), Space: 57.1 MB (75%) - LeetHub

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.java
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/3166-minimum-number-of-groups-to-create-a-valid-assignment.java
@@ -1,53 +1,40 @@
-class Solution {
-
+public class Solution {
     public int minGroupsForValidAssignment(int[] nums) {
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         for (int num : nums) {
             frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
         }
 
-        int maxFrequency = 0;
-        int totalNumbers = nums.length;
-
-        HashMap<Integer, Integer> uniqueFrequencyMap = new HashMap<>();
-        for (int num : nums) {
-            uniqueFrequencyMap.put(num, uniqueFrequencyMap.getOrDefault(num, 0) + 1);
-            maxFrequency = Math.max(maxFrequency, uniqueFrequencyMap.get(num));
+        List<Integer> frequencies = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            frequencies.add(entry.getValue());
         }
 
-        for (int i = maxFrequency; i >= 1; i--) {
-            if (isPossible(i, uniqueFrequencyMap)) {
-                int groups = 0;
-                for (Map.Entry<Integer, Integer> entry : uniqueFrequencyMap.entrySet()) {
-                    int currentNumber = entry.getKey();
-                    int frequency = entry.getValue();
+        Collections.sort(frequencies);
 
-                    int leftOver = frequency % i;
+        int minGroups = nums.length;
+        for (int i = frequencies.get(0); i >= 1; --i) {
+            boolean isValid = true;
+            int totalGroups = 0;
 
-                    groups += (frequency / i);
-                    if (leftOver > 0) groups++;
+            for (int frequency : frequencies) {
+                final int quotient = frequency / i;
+                final int remainder = frequency % i;
+
+                if (remainder > quotient) {
+                    isValid = false;
+                    break;
                 }
-                return groups;
-            }
-        }
-        return -1;
-    }
 
-    public boolean isPossible(int x, HashMap<Integer, Integer> uniqueFrequencyMap) {
-        for (Map.Entry<Integer, Integer> entry : uniqueFrequencyMap.entrySet()) {
-            int currentNumber = entry.getKey();
-            int frequency = entry.getValue();
-
-            int leftOver = frequency % x;
-            int times = frequency / x;
-
-            if (leftOver < x - 1) {
-                int required = (x - 1) - leftOver;
-                if (times >= required) leftOver = x - 1;
+                final int excess = (quotient - remainder) / (i + 1);
+                totalGroups += quotient - excess;
             }
 
-            if (leftOver > 0 && leftOver < x - 1) return false;
+            if (isValid) {
+                minGroups = Math.min(minGroups, totalGroups);
+            }
         }
-        return true;
+
+        return minGroups;
     }
 }
