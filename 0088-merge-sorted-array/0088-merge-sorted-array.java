2020040ae3e9906bca387cd6fede1cbc677142af
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-10-23 13:44:04 +0530

    Time: 0 ms (100%), Space: 41.3 MB (34.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,21 +3,14 @@ class Solution {
         
         m--;
         n--;
-        int i = nums1.length-1;
 
-        for(i = nums1.length-1; i >= 0 && m >= 0 && n >= 0; i--) {
+        for(int i = nums1.length-1; i >= 0 && n >= 0; i--) {
 
-            if(nums1[m] >= nums2[n]) {
+            if(m >= 0 && nums1[m] > nums2[n]) {
                 nums1[i] = nums1[m--];
             } else {
                 nums1[i] = nums2[n--];
             }
-
         }
-
-        while(n >= 0) {
-            nums1[i--] = nums2[n--];
-        }
-
     }
 }
