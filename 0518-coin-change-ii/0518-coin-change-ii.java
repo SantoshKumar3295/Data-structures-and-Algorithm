71974d4b451bb5c6a93b4b1eccedfc1c0740f4b7
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-11-09 09:54:23 +0530

    Time: 12 ms (21.57%), Space: 44.3 MB (53.93%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    Integer memo[][];
+    int coins[];
+
+    public int change(int amount, int[] coins) {
+
+        memo = new Integer[coins.length+1][amount+1];
+        this.coins = coins;
+
+        return rec(0, amount);
+
+    }
+
+    private int rec(int pos, int amount) {
+
+        if(amount == 0) return 1;
+
+        if(pos >= coins.length) return 0;
+
+        if(amount < 0) return 0;
+
+        if(memo[pos][amount] != null) return memo[pos][amount];
+
+        if(coins[pos] > amount) return memo[pos][amount] = rec(pos+1, amount);
+
+        int total = rec(pos, amount-coins[pos]) + rec(pos+1, amount);
+
+        return memo[pos][amount] = total;
+
+    }
+}
