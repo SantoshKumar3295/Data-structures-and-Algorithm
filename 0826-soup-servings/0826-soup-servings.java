060750603e9cd137559c0f8934b92c363e36d685
SantoshKumar3295 <kumarsantosh3295@gmail.com>
2023-11-12 19:08:43 +0530

    Time: 35 ms (43.85%), Space: 271.7 MB (10%) - LeetHub

---
+++ 0826-soup-servings/0826-soup-servings.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    int pick[][] = {{100,0}, {75, 25}, {50, 50}, {25, 75}};
+    double memo[][];
+
+    public double soupServings(int n) {
+
+        if(n > 5000) return 1;
+        
+        memo = new double[n+1][n+1];
+
+        return rec(n,n);
+    }
+
+
+    private double rec(int a, int b) {
+
+        if(a <= 0 && b <= 0) return 0.5;
+
+        if(a <= 0) return 1;
+ 
+        if(b <= 0) return 0;
+
+        if(memo[a][b] != 0) return memo[a][b];
+
+        double ans = 0;
+
+        for(int p[] : pick) {
+            ans += rec(a-p[0], b-p[1]);
+        }
+
+        return memo[a][b] = ans * 0.25;
+
+    }
+
+}
